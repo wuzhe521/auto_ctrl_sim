f63c8f6a6f119e35988b424cca586cfa88910ff6
wuzhe_075 <wuzhe_075@qcraft.ai>
2025-11-06 11:34:30 +0800

    [sim]: First Step

--- controller.py
+++ controller.py
@@ -12,7 +12,8 @@ horizon = 10  # horizon length
 
 max_jerk = 0.5
 max_kappa_rate = 0.05
-
+state_min = np.array([-np.inf, -np.inf, -np.inf, -np.inf])
+state_max = np.array([np.inf, np.inf, np.inf, np.inf])
 
 class MPC_Controller:
     def __init__(self, ts: float, horizon: int):
@@ -20,7 +21,7 @@ class MPC_Controller:
         self.horizon = horizon
         # weight matrix
         self.Q = sp.eye(4)
-        self.R = sp.eye(4)
+        self.R = 1
         self.QN = self.Q
         # reference
         self.ref = []
@@ -29,13 +30,16 @@ class MPC_Controller:
 
     def Update(self, init_status: vehicle_status, trajectory):
         self.init_status = init_status
+        self.ref = trajectory  # update reference
         x0 = init_status.x
         y0 = init_status.y
         theta0 = init_status.theta
+        d_theta0 = init_status.theta - self.ref[0].angle
         kappa0 = init_status.kappa
         v0 = init_status.velocity
-        self.ref = trajectory  # update reference
-        state0 = np.array([x0, y0, v0, kappa0])  # initial state
+        
+        state0 = np.array([x0, y0, d_theta0, kappa0])  # initial state
+        W_0 = np.array([v0 * np.cos(theta0), v0 * np.sin(theta0), 0.0, 0.0])
         Ad = sp.csc_matrix(
             [
                 [1.0, 0.0, -v0 * sin(theta0) * ts, 0.0],
@@ -60,41 +64,57 @@ class MPC_Controller:
             ],
             format="csc",
         )
-        q = np.array([])
-        for i in range(horizon - 1):
+        q = []
+        for i in range(horizon): # 0 -> N-1
             xr = trajectory[i].x
             yr = trajectory[i].y
             dl_theta = trajectory[i].angle - trajectory[0].angle
             kappa_r = trajectory[i].kappa
             state_r = np.array([xr, yr, dl_theta, kappa_r])
             q = np.hstack([q, -self.Q @ state_r])
-        state_rn = np.array([self.ref[-1].x, self.ref[-1].y, self.ref[-1].angle - trajectory[0].angle,
-                             self.ref[-1].kappa])
+        # print("part 1 q size:", q.shape)
+        state_rn = np.array(
+            [
+                self.ref[-1].x,
+                self.ref[-1].y,
+                self.ref[-1].angle - trajectory[0].angle,
+                self.ref[-1].kappa,
+            ]
+        )
+        
         q = np.hstack([q, -self.QN @ state_rn, np.zeros(horizon * nu)])
-        q = np.hstack([q, np.zeros(horizon*nu)])
+        # print("part 2 q size:", q.shape)
         # - linear equations
-        Ax = sp.kron(sp.eye(horizon + 1), -sp.eye(nx)) + sp.kron(sp.eye(horizon + 1, k=-1), Ad)
+        Ax = sp.kron(sp.eye(horizon + 1), -sp.eye(nx)) + sp.kron(
+            sp.eye(horizon + 1, k=-1), Ad
+        )
         Bu = sp.kron(sp.vstack([sp.csc_matrix((1, horizon)), sp.eye(horizon)]), Bd)
         Aeq = sp.hstack([Ax, Bu])
         beq = np.zeros(horizon * nx + horizon * nu)
-        leq = np.hstack([-state0, np.zeros(horizon * nx)])
+        leq = np.hstack([-state0, np.zeros(horizon * nx)]) - np.kron(np.ones(horizon + 1), W_0)
         ueq = leq
         # - inequality constraints
         Aineq = sp.eye((horizon + 1) * nx + horizon)
-        lineq = np.hstack([np.kron(np.ones(horizon + 1), -np.inf), np.kron(np.ones(horizon), u_lower)])
-        uineq = np.hstack([np.kron(np.ones(horizon + 1), np.inf), np.kron(np.ones(horizon), u_upper)])
+        lineq = np.hstack(
+            [np.kron(np.ones(horizon + 1), state_min), np.kron(np.ones(horizon), u_lower)]
+        )
+        uineq = np.hstack(
+            [np.kron(np.ones(horizon + 1), state_max), np.kron(np.ones(horizon), u_upper)]
+        )
+        # print("Aeq size:", Aeq.shape, "Aineq size:", Aineq.shape, "lineq size:", lineq.shape, "uineq size:", uineq.shape)
         # OSQP constraints
         A = sp.vstack([Aeq, Aineq])
         l = np.hstack([leq, lineq])
         u = np.hstack([ueq, uineq])
         # Create an OSQP object
         prob = osqp.OSQP()
-        prob.setup(P, q, A, l, u, warm_starting = True)
+
+        prob.setup(P, q, A, l, u, warm_starting=True, verbose=False)
         res = prob.solve()
         # get control
-        ctrl = res.x[-horizon * nu:-(horizon - 1) * nu]
+        ctrl = res.x[-horizon * nu : -(horizon - 1) * nu]
 
-        return ctrl
+        return ctrl[0]
 
     def get_ref_points(self, ref_points: list):
         self.ref = ref_points
@@ -116,51 +136,7 @@ if __name__ == "__main__":
     control_ref = []
     for i in range(horizon):
         control_ref.append(ref_lin.get_point_from_S(nearest_point, ds[i]))
-    controller.Update(E0Y.get_vehicle_status(), control_ref)
-
+    print(controller.Update(E0Y.get_vehicle_status(), control_ref))
+    
     print("pause")
-    '''
-    N = 3
-    nx = 4
-    nu = 1
-
-    ts = 0.2
-
-
-    Q = sp.eye(2)
-    QN = sp.eye(2)
-    R = sp.eye(2)
-    P = sp.block_diag(
-        [
-            sp.kron(sp.eye(horizon), Q),
-            QN,
-            sp.kron(sp.eye(horizon), R),
-        ]
-    ).toarray()
-
-    print(P)
-
-    xr = np.array([1.0, 0.5])
-
-    q = np.hstack(
-        [
-            np.kron(np.ones(1), -Q @ xr),
-        ]
-    )
-    print(q)
-    q = np.hstack([q, QN @ xr])
-    print(q)
-    Ad = sp.csc_matrix(
-        [
-            [1.0, 0.0, -10.0 * sin(0.02) * ts, 0.0],
-            [0.0, 1.0, 10.0 * cos(0.02) * ts, 0.0],
-            [0.0, 0.0, 1.0, 10.0 * ts],
-            [0.0, 0.0, 0.0, 1.0]
-        ]
-    )
-    print("Ad:", Ad.toarray())
-    Ax = sp.kron(sp.eye(N + 1), -sp.eye(nx)) + sp.kron(sp.eye(N + 1, k=-1), Ad)
-    print("Ax:", Ax.toarray())
-    Aineq = sp.eye((3 + 1) * 2 + 3 * 1).toarray()
-    print(Aineq)
-    '''
+    
--- draw_env.py
+++ draw_env.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from referenceline import reference_line
-
-
+from controller import MPC_Controller, ts, horizon
 def Coordinate_transform(x, y, x0, y0, angle):
     rotate_x = (x) * np.cos(angle) + (y) * np.sin(angle)
     rotate_y = -(x) * np.sin(angle) + (y) * np.cos(angle)
@@ -21,6 +20,7 @@ if __name__ == "__main__":
     E0Y = vehicle_model("E0Y", 0.0, 0.005, 10.0, 0.5, -5, -5)
     ref_lin = reference_line(-5.0, 0.005, 0.0005)
     trajectory = ref_lin.get_ref_points(preview_t * E0Y.velocity)
+    controller = MPC_Controller(ts, horizon)
     print(ref_lin.points[0])
     fig, ax = plt.subplots()
     ax.set_xlim(-10, 150)
@@ -35,31 +35,34 @@ if __name__ == "__main__":
         ax.set_xlim(-10, 100)
         ax.set_ylim(-10, 10)
         ax.set_aspect("equal")
-        preview_s = E0Y.velocity * preview_t
 
+        loc = E0Y.position()
+        
         rect = patches.Polygon(
-            E0Y.position(),
+            loc,
             linewidth=2,
             edgecolor="blue",
             facecolor="lightblue",
             alpha=0.7,
         )
         ax.add_patch(rect)
+    
         nearest_point = ref_lin.get_nearest_point(E0Y.X, E0Y.Y)  # get nearest point
-        ds = [E0Y.velocity * control_ts * i for i in range(horizon)]
-        # print(ds)
+        ds = [E0Y.velocity * ts * i for i in range(horizon)]
         control_ref = []
         for i in range(horizon):
-            control_ref.append(
-                ref_lin.get_point_from_S(nearest_point, ds[i])
-            )
+            control_ref.append(ref_lin.get_point_from_S(nearest_point, ds[i]))
+        ctrl = controller.Update(E0Y.get_vehicle_status(), control_ref)
+        print("Control : ", ctrl)
         control_ref_x = [pt.x for pt in control_ref]
         control_ref_y = [pt.y for pt in control_ref]
         
         plt.scatter(control_ref_x, control_ref_y, s=5, c="b")
         plt.scatter(traj_x, traj_y, s=1, c="r")  # draw reference line
         plt.scatter(nearest_point.x, nearest_point.y, s=5, c="g")  # draw nearest point
-        E0Y.kinematic_Update(0, 0.2)  # update kinematic model
+        
+        #E0Y.kinematic_Update(ctrl, ts)  # update kinematic model
+        E0Y.kinematic_Update(0, ts)
         plt.pause(0.5)
         ax.cla()
     # 关闭交互模式
